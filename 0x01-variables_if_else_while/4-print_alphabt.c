2150efab09fa4fc861a6958c9517852d46654d8f
ahmedel-gendy <ahmedayman0794@gmail.com>
2022-07-03 14:11:27 -0700

    printing alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+/* prints the alphabet in lowercase, followed by a new line. */
+/* Print all the letters except q and e. */
+/* Use putchar only. */
+/* All your code should be in the main function. */
+
 /**
- *main - print lowercase alpha a-z but remove 'q' and 'e'
- *Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-char alpha = 'a';
-while (alpha <= 'z')
+/* initalize variables */
+char ch;
+/* prints the alphabet in lowercase, followed by a new line. */
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-if ((alpha != 'q') && (alpha != 'e')
-{
-putchar(alpha);
-}
-alpha++;
+/* Print all the letters except q and e. */
+if ((ch != 'q') && (ch != 'e'))
+putchar(ch);
 }
 putchar('\n');
 return (0);
